7ceb93a9808bbbe8894f70e3b749b62ac3158d0a
Ali Faal <faal.ali.faal@gmail.com>
2023-08-28 16:15:19 +0330

    Modify keywordinfo.cs to keywordinfobase for using inheritance in class library

---
+++ KeywordDocumentationLibrary/KeywordInfoBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeywordDocumentationLibrary
+{
+    public abstract class KeywordInfoBase
+    {
+        public string Keyword { get; protected set; }
+        public string Description { get; protected set; }
+        public KeywordInfoBase(string description)
+        {
+            Description = description;
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Keyword: {Keyword}");
+            Console.WriteLine($"Description: {Description}");
+        }
+    }
+}
