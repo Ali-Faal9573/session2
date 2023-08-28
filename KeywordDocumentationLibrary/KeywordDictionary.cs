9bfaa70d72cc1c6e801f4ce058c7cd93e351ce7f
Ali Faal <faal.ali.faal@gmail.com>
2023-08-28 16:25:33 +0330

    modify KeywordDictionary class in library for using inheritance and keywords subclases

--- KeywordDocumentationLibrary/KeywordDictionary.cs
+++ KeywordDocumentationLibrary/KeywordDictionary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KeywordDocumentationLibrary.Keywords;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +7,33 @@ using System.Threading.Tasks;
 
 namespace KeywordDocumentationLibrary
 {
-    public class KeywordDictionary
+    public static class KeywordDictionary
     {
-        private static readonly Dictionary<string, string> Keywords = new Dictionary<string, string>
+        private static readonly Dictionary<string, Type> KeywordMappings = new Dictionary<string, Type>
         {
-            { "abstract", "The 'abstract' keyword is used to define an abstract class or method. Abstract classes cannot be instantiated and are meant to be subclassed." },
-            { "async", "The 'async' keyword is used to indicate that a method contains asynchronous code and can use the 'await' keyword to pause execution without blocking the thread." },
-            { "const", "The 'const' keyword is used to declare a constant field that cannot be modified after it is assigned a value." },
-            { "event", "The 'event' keyword is used to declare an event, which allows objects to communicate and react to actions or changes." },
+            { "abstract", typeof(AbstractKeywordInfo) },
+            { "async", typeof(AsyncKeywordInfo) },
+            { "const", typeof(ConstKeywordInfo) },
+            { "event", typeof(EventKeywordInfo) },
+            { "extern", typeof(ExternKeywordInfo) },
+            { "new", typeof(NewKeywordInfo) },
+            { "override", typeof(OverrideKeuwordInfo) },
+            { "partial", typeof(PartialKeywordInfo) },
+            { "readonly", typeof(ReadonlyKeywordInfo) },
+            { "sealed", typeof(SealedKeywordInfo) },
+            { "static", typeof(StaticKeywordInfo) },
+            { "unsafe", typeof(UnsafeKeywordInfo) },
+            { "virtual", typeof(VirtualKeywordInfo) },
+            { "volatile", typeof(VolatileKeywordInfo) }
         };
 
-        public static KeywordInfo GetKeywordInfo(string keyword)
+        public static KeywordInfoBase GetKeywordInfo(string keyword)
         {
-            if (Keywords.ContainsKey(keyword))
+            if (KeywordMappings.TryGetValue(keyword.ToLower(), out Type keywordType))
             {
-                return new KeywordInfo(keyword, Keywords[keyword]);
+                return (KeywordInfoBase)Activator.CreateInstance(keywordType);
             }
+
             return null;
         }
     }
