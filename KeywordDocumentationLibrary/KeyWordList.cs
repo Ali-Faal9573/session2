8bc1a8c86738328ad2f33c8f3d2cf2f39daa11ef
Ali Faal <faal.ali.faal@gmail.com>
2023-08-28 16:17:47 +0330

    create a class in library for showing the list of keyword to user

---
+++ KeywordDocumentationLibrary/KeyWordList.cs
@@ -0,0 +1,11 @@
+﻿namespace KeywordDocumentationLibrary
+{
+    public static class KeyWordList
+    {
+
+        public static string showstartingtext()
+        {
+            return "\nEnter a keyword to get its explanation (or 'exit' to quit): \nabstract , async ,  const , event , extern , new , override , partial , readonly , sealed , static , unsafe , virtual and volatial";
+        }
+    }
+}
